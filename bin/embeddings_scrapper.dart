8f84d3441ba5f53fc37df6536e9bd71cbe8fe8f9
jideguru <guruliciousjide@gmail.com>
2023-06-05 13:45:42 +0100

    added get countries

--- bin/embeddings_scrapper.dart
+++ bin/embeddings_scrapper.dart
@@ -1,23 +1,42 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:embeddings_scrapper/embeddings_scrapper.dart'
-    as embeddings_scrapper;
 import 'package:dotenv/dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart'; // Contains a client for making API calls
 import 'package:html/parser.dart'; // Contains HTML parsers to generate a Document object
 import 'package:html/dom.dart'; // Contains DOM related classes for extracting data from elements
 
 void main(List<String> arguments) async {
-  var env = DotEnv(includePlatformEnvironment: true)..load();
-  final openAIKey = env['OPENAIKEY'];
+  int currentFileCount = 101;
+
+  // List<String> phrases = await getCountriesList();
+  List<String> phrases = await scrapeGenerator();
+  if (phrases.isNotEmpty) {
+    for (String phrase in phrases) {
+      String text = phrase.trim();
+      print(text);
+      List<double>? embedding = await getEmbeddingFromOpenAI(text);
+      String json = '''
+{
+      "text": "$text",
+      "embedding": $embedding
+    }
+''';
+      currentFileCount += 1;
+      // Create assets folder
+      Directory('assets').create();
+      File file = await File('assets/$currentFileCount.json').create();
+      file.writeAsString(json);
+    }
+  }
+}
+
+Future<List<String>> scrapeGenerator() async {
   final randomPhraseUrl =
       'https://www.coolgenerator.com/random-phrase-generator'; // https://www.coolgenerator.com/recipe-generator
   final res = await http.get(Uri.parse(randomPhraseUrl));
   var document = parse(res.body);
-  int currentFileCount = 1;
-  List phrases = [];
+  List<String> phrases = [];
   List<Element> phraseElements = document.getElementsByClassName('font-18');
   for (Element element in phraseElements) {
     List<Element> phraseSpans = element.getElementsByTagName('span');
@@ -26,28 +45,29 @@ void main(List<String> arguments) async {
       phrases.add(phraseSpans[0].text);
     }
   }
-
-  for (String phrase in phrases) {
-    String text = phrase.trim();
-    print(text);
-    List<double>? embedding =
-        await getEmbeddingFromOpenAI(text, openAIKey ?? '');
-    String json = '''
-{
-      "text": "$text",
-      "embedding": $embedding
-    }
-''';
-    currentFileCount += 1;
-    // Create assets folder
-    Directory('assets').create();
-    File file = await File('assets/$currentFileCount.json').create();
-    file.writeAsString(json);
-  }
+  return phrases;
 }
 
-Future<List<double>?> getEmbeddingFromOpenAI(
-    String text, String openAIKey) async {
+Future<List<String>> getCountriesList() async {
+  var url = Uri.parse('https://restcountries.com/v3.1/all');
+  var response = await http.get(
+    url,
+    headers: {'Content-Type': 'application/json'},
+  );
+  List<String> phrases = [];
+  if (response.statusCode == 200) {
+    List<dynamic> countries = jsonDecode(response.body);
+    for (var country in countries) {
+      print(country['name']['common']);
+      phrases.add(country['name']['common']);
+    }
+  }
+  return phrases;
+}
+
+Future<List<double>?> getEmbeddingFromOpenAI(String text) async {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+  final openAIKey = env['OPENAIKEY'];
   var url = Uri.parse('https://api.openai.com/v1/embeddings');
   var response = await http.post(
     url,
